8311895e3adcda3e5e20374828fc26e36f4d86f2
Makarand Nikam <64835912+mack-1999@users.noreply.github.com>
2025-03-07 14:09:11 +0530

    Implemented Eureka Server

--- HotelService/src/main/java/com/hotel/service/HotelServiceApplication.java
+++ HotelService/src/main/java/com/hotel/service/HotelServiceApplication.java
@@ -3,11 +3,14 @@ package com.hotel.service;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 //@EnableDiscoveryClient
 //@EnableCaching
+@EnableEurekaServer
 public class HotelServiceApplication {
 
 	public static void main(String[] args) {
